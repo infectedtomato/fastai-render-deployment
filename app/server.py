4ea5f1d927c8e3d2adebb708ac1ac10d5af02ddb
Animesh Deharia <deharia.animesh@gmail.com>
2020-05-13 12:10:26 +0530

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,10 +9,10 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
-export_file_name = 'export.pkl'
+export_file_url = 'https://www.dropbox.com/s/ttn7y3ixlhn9ivj/multi-class_classification.pkl?dl=1'
+export_file_name = 'multi-class_classification.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['1', '10', '100', '20', '200', '2000', '5', '50', '500', 'GBP', 'INR', 'USD']
 path = Path(__file__).parent
 
 app = Starlette()
